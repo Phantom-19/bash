b262552b1b03320a58fb8353d82224c72b003502
Phantom-19 <63906142+Phantom-19@users.noreply.github.com>
2020-05-21 17:23:55 +0000

    Update maj.sh

--- maj.sh
+++ maj.sh
@@ -1,13 +1,15 @@
+echo ""
 toilet -f pagga  BASH  | lolcat
 echo ""
-echo "mise à jour bash........." | lolcat
+echo "Mise a jour de BASH en cours........." | lolcat
 echo ""
-git pull origin master 
-echo "mise à jour de bash terminer........." | lolcat
+git pull origin master
 echo ""
-figlet -f small Mise à jour ! | lolcat
+echo "Mise à jour de BASH terminer........." | lolcat
 echo ""
-figlet -f small Mr Hacker K | lolcat
+figlet -f small Mise a jour ! | lolcat
 echo ""
 echo "[ Mise à jour terminée..]"  | lolcat
+echo ""
+toilet -f pagga  Mr Hacker K  | lolcat
 echo ""
