f0a344744338d1bed6b5232fd440972ac9fab257
Phantom-19 <63906142+Phantom-19@users.noreply.github.com>
2020-05-21 17:08:49 +0000

    Update and rename sudo1 to maj.sh

---
+++ maj.sh
@@ -0,0 +1,12 @@
+echo "BASH."
+echo ""
+echo "mise à jour terminée bash........." 
+echo ""
+git pull origin master 
+echo ""
+figlet -f small mise à jour ! 
+echo ""
+figlet -f small Mr Hacker_K ! 
+echo ""
+echo "[ mise à jour terminée..]"  
+echo ""
