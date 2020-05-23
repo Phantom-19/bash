ba182c064126e63c6a7d642d7f6d82d535e81a22
Phantom-19 <63906142+Phantom-19@users.noreply.github.com>
2020-05-23 14:16:57 +0000

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,3 +1,15 @@
+
+m = '\x1b[1;91m'
+h = '\x1b[1;92m'
+k = '\x1b[1;93m'
+b = '\x1b[1;94m'
+u = '\x1b[1;95m'
+c = '\x1b[1;96m'
+p = '\x1b[0m'
+i = '\x1b[1;90m'
+v = '\x1b[1;38;5;198m'
+j = '\x1b[1;38;5;208m'
+w = (m, v, j, p, k, b, u, c)
 clear
 toilet -f pagga  IP Scan | lolcat
 echo ""
@@ -14,7 +26,7 @@ echo "[!] Avant d'executer essayez d'utiliser un VPN,\npour bloquer le site Web 
 echo ""
 echo "[+] Site Web a scanner exemple : (www.faxel.com) [+]" $cible | lolcat
 echo ""
-read -p "\033[31;1m[\033[36;1mMr Hacker_K\033[31;1m]\033[33;1m~~>\033[32;1m"  cible
+read -p "[Mr Hacker_K]~~>"  cible | lolcat
 echo ""
 echo "Patientez...................." | lolcat
 echo ""
