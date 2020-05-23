7c8efbc016a6a25b035cef92a5e0e295b79e66ed
Phantom-19 <63906142+Phantom-19@users.noreply.github.com>
2020-05-23 14:11:33 +0000

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -3,12 +3,20 @@ toilet -f pagga  IP Scan | lolcat
 echo ""
 echo "=====================
     Coder par: Faxel
-======================" | lolcat
+=====================" | lolcat
+echo ""
+echo "\033[34;1m"
+cal
+date
+sleep 1
 echo ""
 echo "[!] Avant d'executer essayez d'utiliser un VPN,\npour bloquer le site Web de l'opérateur  [!]" | lolcat
 echo ""
-echo "[+] Site Web  (www.faxel.com) :  " $cible | lolcat
+echo "[+] Site Web a scanner exemple : (www.faxel.com) [+]" $cible | lolcat
 echo ""
-read -p "[Mr hacker_K]~~> "  cible
+read -p "\033[31;1m[\033[36;1mMr Hacker_K\033[31;1m]\033[33;1m~~>\033[32;1m"  cible
+echo ""
+echo "Patientez...................." | lolcat
+echo ""
 sleep 1
 ping $cible
