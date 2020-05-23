b5d55a37ff26b5cfbd9d83f1fc241971dc834b75
Phantom-19 <63906142+Phantom-19@users.noreply.github.com>
2020-05-23 14:29:06 +0000

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -9,13 +9,11 @@ date
 sleep 1
 echo ""
 echo "\033[31;1m[!]\033[37;1m Avant d'executer essayez d'utiliser un\033[35;1m VPN\033[37;1m,\npour bloquer le site Web de l'opérateur\033[31;1m [!]" 
-echo ""
 echo "[+] Site Web a scanner exemple : (www.faxel.com) [+]" $cible | lolcat
 echo ""
 echo "\033[36;1m"
 read -p "[Mr Hacker_K]~~>  " cible 
-echo ""
-echo "Patientez...................." | lolcat
+echo "\033[38;5;298mPatientez\033[33;1m...................." | lolcat
 echo ""
 echo "\033[37;1m"
 sleep 1
