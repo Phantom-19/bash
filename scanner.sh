d07933ed529316b42934480f845b5ff90a328990
Phantom-19 <63906142+Phantom-19@users.noreply.github.com>
2020-09-01 18:35:11 +0000

    Update and rename sc.sh to scanner.sh

--- scanner.sh
+++ scanner.sh
@@ -10,7 +10,7 @@ echo ""
 echo "\033[31;1m[!]\033[37;1m Avant d'executer essayez d'utiliser un\033[35;1m VPN\033[37;1m,\npour bloquer le site Web de l'opérateur\033[31;1m [!]" 
 echo "[+] Site Web a scanner exemple : (www.faxel.com) [+]" $cible | lolcat
 echo "\033[36;1m"
-read -p "[Mr Hacker_K]~~>  " cible 
+read -p "[Mr Faxel]~~>  " cible 
 echo ""
 echo "\033[1;92mPatientez\033[1;93m...................." | lolcat
 echo "\033[37;1m"
