8a42cf5a28a5029453f54fdbdc65cf6814ef8bbd
Faxel <63906142+Phantom-19@users.noreply.github.com>
2020-09-26 11:22:10 +0000

    Create fonction.js

---
+++ avatar/fonction.js
@@ -0,0 +1,38 @@
+https://www.asciiart.eu/
+http://patorjk.com/software/taag/ (solo banners de texto)
+https://asciiart.website/
+http://www.asciiworld.com/
+https://textfancy.com
+https://ascii.co.uk/
+
+
+PS1='\n \e[1;36m>>\e[1;32mTunombre\e[1;35m@\e[1;33mtermux\e[1;36m<<\e[1;32m [\e[1;33m \d\e[1;32m]\e[1;32m [\e[1;33m\t \e[1;32m] 
+|\e[1;31m[\e[1;33m \s \h \e[1;31m]
+|__\e[1;31m[\e[1;33m \w \e[1;31m]
+|
+\e[1;31m|__\e[1;31m[\e[1;36m > \e[1;31m]\e[1;39m '" >> 
+
+
+#LES FONCTIONS:
+# \a un caractère ASCII de l'anneau
+# \d la date actuelle au format jour_sem mois jour, dim 18 novembre
+# \e un caractère d'échappement ASCII
+# \h le nom de l'équipe jusqu'au premier., exemple linuxtotal de linuxtotal.com.mx
+# \H le nom de l'équipe
+# \n nouvelle ligne
+# \r retour chariot, entrez
+# \s le nom du shell
+# \t l'heure actuelle au format 24 heures HH: MM: SS
+# \T l'heure actuelle au format 12 heures HH: MM: SS
+# \@ l'heure actuelle est de 12 heures avec am / pm
+# \u l'utilisateur actuel
+# \v la version bash
+# \V le numéro de version du lot, version + patch
+# \w le répertoire de travail actuel, chemin
+# \W le nom du répertoire courant
+# \! le numéro dans l'historique des commandes
+# \# le numéro de commande de cette commande
+# \$ si l'utilisateur est root (UID = 0) un # est indiqué, un utilisateur normal $
+# \\ diagonale
+# \ [début d'une séquence de caractères non imprimables
+# \] fin de la séquence de caractères non imprimables
